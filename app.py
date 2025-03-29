87dcd782e91e21bee05de7c54f5c56048aab8beb
Soumodip Ghosh <soumodip615c@gmail.com>
2025-03-30 01:39:01 +0530

    Added /run_model route

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
+from model_utils import run_prediction_and_plot
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template("index.html", image_path="static/plot.png")
+def index():
+    image_path = "static/plot.png"
+    run_prediction_and_plot()
+    return render_template("index.html", image_path=image_path)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/run_model')
+def run_model():
+    run_prediction_and_plot()
+    return "Model run successfully and plot updated!"
